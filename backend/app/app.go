a423be54cbd1f00cc7be8c0b09d44dfd5d6016f6
EdwardSushkov <sushkov2007@gmail.com>
2026-03-17 21:18:02 +0300

    Project setup

---
+++ backend/app/app.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("GO_PORT")), nil))
+}
